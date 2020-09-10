56261788ec2d2f29eb128920938b20ef2c12f44c
bk0218 <sbk02@naver.com>
2020-09-10 15:10:08 +0900

    Update Largest-perimeter-triangle.py
    Runtime: 208 ms, faster than 65.56% of Python3 online submissions for Largest Perimeter Triangle.
    Memory Usage: 15 MB, less than 55.91% of Python3 online submissions for Largest Perimeter Triangle.

--- 9월/보경/Largest-perimeter-triangle.py
+++ 9월/보경/Largest-perimeter-triangle.py
@@ -5,4 +5,5 @@ class Solution:
         for a , b , c in zip(A, A[1:], A[2:]):
             if b + c > a:
                 return a + b + c
-        return 0
+        return 0        return 0
+        
