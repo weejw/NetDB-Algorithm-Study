dab23ca81686515e2bbd6926bd3bb6e1694315a2
bk0218 <sbk02@naver.com>
2020-09-10 15:05:51 +0900

    Create Largest-perimeter-triangle.py

---
+++ 9월/보경/Largest-perimeter-triangle.py
@@ -0,0 +1,8 @@
+class Solution:
+    def largestPerimeter(self, A):
+        A.sort(reverse=True)
+        
+        for a , b , c in zip(A, A[1:], A[2:]):
+            if b + c > a:
+                return a + b + c
+        return 0
